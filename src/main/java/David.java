11e8f89bd74ac917cabf64f49e53cadfe76b65ff
DavidShenJingtao <E1297756@u.nus.edu>
2025-08-20 23:58:24 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/David.java
@@ -0,0 +1,14 @@
+public class David {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);*/
+        String newline = "----------------------------------------";
+        System.out.println(newline + "\n" + "Hello! I'm David.\n"
+            + "What can I do for you?\n\n" + newline + "\n"
+            + "Bye. Hope to see you again soon!\n\n" + newline);
+    }
+}
