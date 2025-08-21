de8bbd5ac28405d9fea356af076dc503cc35d515
DavidShenJingtao <E1297756@u.nus.edu>
2025-08-21 16:30:14 +0800

    Level 3: add the ability to mark tasks as done, and add the ability to change the status back to not done.

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class David {
@@ -5,10 +6,11 @@ public class David {
     private static final String newline = s.repeat(60);
     private static int totalTasks = 0;
 
-    public static void printList(String[] list) {
-        System.out.print(newline.indent(4));
+    public static void printList(ArrayList<? extends Task> list) {
+        String start = newline + "\n Here are the tasks in your list: ";
+        System.out.print(start.indent(4));
         for (int i = 0; i < totalTasks; i++) {
-            String msg = String.format(" %d. %s", i + 1, list[i]);
+            String msg = String.format(" %d. %s", i + 1, list.get(i));
             System.out.print(msg.indent(4));
         }
         System.out.print(newline.indent(4));
@@ -22,20 +24,42 @@ public class David {
         System.out.println(welcome.indent(4));
 
         //user input
-        String[] list = new String[100];
-        int i = 0;
+        ArrayList<Task> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+
         while (!s.equals("bye")) {
+            String[] strarr = s.split(" ", 2);
+
             if (s.equals("list")) {
                 printList(list);
+
+            } else if (strarr[0].equals("mark")) {
+                int index = Integer.valueOf(strarr[1]) - 1;
+                Task t = list.get(index);
+                t.markAsDone();
+                String mark = newline
+                        + "\n Nice! I've mark this task as done:\n  "
+                        + t + "\n" + newline + "\n";
+                System.out.println(mark.indent(4));
+
+            } else if (strarr[0].equals("unmark")) {
+                int index = Integer.valueOf(strarr[1]) - 1;
+                Task t = list.get(index);
+                t.markAsUndone();
+                String unmark = newline
+                        + "\n OK, I've marked this task as not done yet:\n  "
+                        + t + "\n" + newline + "\n";
+                System.out.println(unmark.indent(4));
+
             } else {
-                list[i] = s;
-                i++;
+                Task t = new Task(s);
+                list.add(t);
                 totalTasks++;
-                String msg = newline + "\n added: " + s
+                String msg = newline + "\n added: " + t
                         + "\n" + newline + "\n";
                 System.out.println(msg.indent(4));
+
             }
             s = sc.nextLine();
         }
