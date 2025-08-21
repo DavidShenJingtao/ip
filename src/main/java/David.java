be8fb00d36659f77356db54259ca83fef33f2628
DavidShenJingtao <E1297756@u.nus.edu>
2025-08-21 15:28:38 +0800

    Level-2: add a list to store texts entered by the user, display them back when requested

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -1,23 +1,45 @@
 import java.util.Scanner;
 
 public class David {
+    private static final String s = "_";
+    private static final String newline = s.repeat(60);
+    private static int totalTasks = 0;
+
+    public static void printList(String[] list) {
+        System.out.print(newline.indent(4));
+        for (int i = 0; i < totalTasks; i++) {
+            String msg = String.format(" %d. %s", i + 1, list[i]);
+            System.out.print(msg.indent(4));
+        }
+        System.out.print(newline.indent(4));
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         //welcome page
-        String newline = "----------------------------------------";
-        String welcome = newline + "\nHello! I'm David.\n"
-                    + "What can I do for you?\n\n" + newline + "\n";
+        String welcome = newline + "\n Hello! I'm David.\n" +
+                    " What can I do for you?\n" + newline + "\n";
         System.out.println(welcome.indent(4));
 
         //user input
+        String[] list = new String[100];
+        int i = 0;
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         while (!s.equals("bye")) {
-            String msg = newline + "\n" + s
-                        + "\n\n" + newline + "\n";
-            System.out.println(msg.indent(4));
+            if (s.equals("list")) {
+                printList(list);
+            } else {
+                list[i] = s;
+                i++;
+                totalTasks++;
+                String msg = newline + "\n added: " + s
+                        + "\n" + newline + "\n";
+                System.out.println(msg.indent(4));
+            }
             s = sc.nextLine();
         }
-        String bye = newline + "\nBye. Hope to see you again soon!\n\n"
+        String bye = newline + "\n Bye. Hope to see you again soon!\n"
                     + newline + "\n";
         System.out.println(bye.indent(4));
         sc.close();
