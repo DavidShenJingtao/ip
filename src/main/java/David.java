da5b014842b77f5f612aecaa302950dedff63670
DavidShenJingtao <E1297756@u.nus.edu>
2025-08-22 12:32:05 +0800

    Level-6: Add support for deleting tasks.

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -67,6 +67,25 @@ public class David {
         System.out.println(format(msg));
     }
 
+    public static void delete(String s) throws DavidException {
+        String[] strarr = s.split(" ");
+        if (strarr.length <= 1 || strarr.length > 2 || !isInteger(strarr[1])) {
+            throw new NumberException("the value you entered after delete");
+        }
+        int index = Integer.valueOf(strarr[1]) - 1;
+        if (index < 0 || index > list.size() - 1) {
+            throw new IndexException("the value you entered after delete");
+        }
+        Task t = list.get(index);
+        list.remove(index);
+        totalTasks--;
+        String task = (totalTasks > 1) ? "tasks" : "task";
+        String msg = "Noted, I've removed this task:\n  " + t
+                    + "\n Now you have " + totalTasks + " "
+                    + task + " in the list.";
+        System.out.println(format(msg));
+    }
+
     public static Task parseTask(String[] strarr) throws DavidException {
         String type = strarr[0];
         if (!type.equals("todo") && !type.equals("deadline")
@@ -85,14 +104,14 @@ public class David {
                 String[] by = strarr[1].split(" /by ", 2);
                 if (by.length < 2) {
                     String m = "the correct format of deadline should be: " +
-                            "deadline [task name] /by [time]";
+                            "deadline [task name] /by [time].";
                     throw new FormatException(m);
                 }
                 description = by[0] + " (by: " + by[1] + ")";
                 return new Deadline(description);
             case "event":
                 String m = "the correct format of event should be: " +
-                        "event [task name] /from [start time] /to [end time]";
+                        "event [task name] /from [start time] /to [end time].";
                 String[] from = strarr[1].split(" /from ", 2);
                 if (from.length < 2) {
                     throw new FormatException(m);
@@ -136,6 +155,17 @@ public class David {
             } else if (strarr[0].equals("unmark")) {
                 try {
                     unmark(s);
+                } catch (NumberException e1) {
+                    System.out.println(format(e1.getMessage()));
+                } catch (IndexException e2) {
+                    System.out.println(format(e2.getMessage()));
+                } catch (DavidException e) {
+                    System.out.println(format(e.getMessage()));
+                }
+
+            } else if (strarr[0].equals("delete")) {
+                try {
+                    delete(s);
                 } catch (NumberException e1) {
                     System.out.println(format(e1.getMessage()));
                 } catch (IndexException e2) {
