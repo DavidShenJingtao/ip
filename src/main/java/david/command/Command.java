45b0080a4eed1e2511c36fee0d7150fbe1f54ec0
DavidShenJingtao <E1297756@u.nus.edu>
2025-08-31 10:44:55 +0800

    Reformat the code

--- src/main/java/david/command/Command.java
+++ src/main/java/david/command/Command.java
@@ -11,22 +11,11 @@ import david.ui.TaskList;
 import david.ui.Ui;
 
 public abstract class Command {
-    /**
-     * Executes the command.
-     *
-     * @param tasks The list of tasks to operate on.
-     * @param ui User interface for displaying messages.
-     * @param storage Save changes in the list.
-     * @throws DavidException if something goes wrong.
-     */
+
     public abstract void execute(TaskList tasks, Ui ui,
                                     Storage storage) throws DavidException;
 
-    /**
-     * Checks whether the command will exit the program.
-     */
     public boolean isExit() {
         return false;
     }
-
 }
--- src/main/java/david/ui/Formatter.java
+++ src/main/java/david/ui/Formatter.java
@@ -4,12 +4,6 @@ public class Formatter {
     private static final String s = "_";
     public static final String NEWLINE = s.repeat(100);
 
-    /**
-     * Formats the output.
-     * 
-     * @param s
-     * @return formmatted output.
-     */
     public static String format(String output) {
         String msg = NEWLINE + "\n " + output + "\n" + NEWLINE + "\n";
         return msg.indent(4);
